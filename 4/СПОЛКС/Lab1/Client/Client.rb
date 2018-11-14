2c94f296746a4803a03f5ba82639e86726b77812
SergeiKhaladtsou <SergeiKhaladtsou@gmail.com>
2018-11-14 17:21:55 +0300

    upload/download correction

--- 4/СПОЛКС/Lab1/Client/Client.rb
+++ 4/СПОЛКС/Lab1/Client/Client.rb
@@ -1,4 +1,6 @@
 require "socket"
+
+
 include Socket::Constants
 
 #client = TCPSocket.open "#{address.strip}", 2000
@@ -8,69 +10,88 @@ address = gets
 
 client = Socket.new(AF_INET, SOCK_STREAM, 0)
 sockaddr = Socket.pack_sockaddr_in(2000, address.strip!)
+
+client.setsockopt(Socket::SOL_SOCKET, Socket::SO_KEEPALIVE, 30)
+client.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_KEEPCNT, true)
+client.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_KEEPIDLE, true)
+client.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_KEEPINTVL, true)
+
 client.connect(sockaddr)
 
 loop do
-  puts "1. Time"
-  puts "2. Echo"
-  puts "3. Upload"
-  puts "4. Download"
-  puts "5. Disconnect"
-  puts "6. Close server and disconnect"
-  command = gets
-  command.strip!
-  client.puts command.to_i.to_s 2
-  case command.to_i
-  when 1
-    start_time = Time.now
-    line = client.gets
-    line.strip!
-    puts line
-  when 2
-    print "Input strintg "
-    client.puts "ECHO #{line = gets}"
-    puts "Answer: #{client.gets}"
-  when 3
-    print "Input file name: "
-    file_name = gets
-    file_name.strip!
-    client.puts file_name
-    unless File.exist?(file_name)
-      puts "File don't exits!"
-      client.puts 0
-    else
+  begin
+    puts "1. Time"
+    puts "2. Echo"
+    puts "3. Upload"
+    puts "4. Download"
+    puts "5. Disconnect"
+    puts "6. Close server and disconnect"
+    command = gets
+    command.strip!
+    client.puts command.to_i.to_s 2
+    case command.to_i
+    when 1
       start_time = Time.now
-      file = File.open file_name, "rb"
-      client.puts file.size
-      data = file.read
-      client.puts data
-      puts "Upload time: #{Time.now - start_time}"
-      file.close
-    end
-  when 4
-    print "Input file name: "
-    file_name = gets
-    file_name.strip!
-    client.puts file_name
-    check_size = client.gets
-    check_size.strip!
-    unless check_size.to_i == 0
-      start_time = Time.now
-      file = File.open file_name, "wb"
-      while check_size.to_i > file.size
-        data = client.gets
-        file.write data
+      line = client.gets
+      line.strip!
+      puts line
+    when 2
+      print "Input strintg "
+      client.puts "ECHO #{line = gets}"
+      puts "Answer: #{client.gets}"
+    when 3
+      print "Input file name: "
+      file_name = gets
+      file_name.strip!
+      client.puts file_name
+      unless File.exist?(file_name)
+        puts "File don't exits!"
+        client.puts 0
+      else
+        start_time = Time.now
+        file = File.open file_name, "rb"
+        client.puts file.size
+        data = file.read
+        client.puts data
+        puts "Upload time: #{Time.now - start_time}"
+        file.close
       end
-      file.close
-      puts "Upload time: #{Time.now - start_time}"
-    else
-      puts "File don't exist!"
+    when 4
+      print "Input file name: "
+      file_name = gets
+      file_name.strip!
+      client.puts file_name
+      check_size = client.gets
+      check_size.strip!
+      unless check_size.to_i == 0
+        start_time = Time.now
+        file = File.open file_name, "wb"
+        while check_size.to_i > file.size
+          data = client.gets
+          file.write data
+        end
+        file.close
+        puts "Upload time: #{Time.now - start_time}"
+      else
+        puts "File don't exist!"
+      end
+    when 5
+      client.close
+      break
+    when 6
+      client.close
+      break
     end
-  when 5
-    client.close
-    break
-  when 6
-    client.close
-    break
+  rescue => err
+    puts err
+    puts "Connection error! Reconnect? (y)"
+    ans = gets
+    unless ans.strip! == "y"
+      exit
+    else
+      redo
+      #client.connect(sockaddr)
+      #retry
+    end
   end
 end
--- 4/СПОЛКС/Lab1/Server/Server.rb
+++ 4/СПОЛКС/Lab1/Server/Server.rb
@@ -1,4 +1,6 @@
 require "socket"
+
+
 include Socket::Constants
 
 #socket = TCPServer.open "#{address.strip!}", 2000
@@ -9,7 +11,7 @@ address = gets
 server = Socket.new(AF_INET, SOCK_STREAM, 0)
 server.bind(Addrinfo.tcp(address.strip!, "2000"))
 
-server.setsockopt(Socket::SOL_SOCKET, Socket::SO_KEEPALIVE, true)
+server.setsockopt(Socket::SOL_SOCKET, Socket::SO_KEEPALIVE, 30)
 server.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_KEEPCNT, true)
 server.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_KEEPIDLE, true)
 server.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_KEEPINTVL, true)
@@ -19,51 +21,68 @@ p server.connect_address
 loop do
   server.listen(5)
   socket = server.accept
-  loop do
-    command = socket[0].gets
-    if command
-      case command.to_i 2
-      when 1
-        puts Time.now
-        socket[0].puts "Server time #{Time.now}"
-      when 2
-        line = socket[0].gets
-        puts line.strip!
-        socket[0].puts line.delete("ECHO ")
-      when 3
-        file_name = socket[0].gets
-        file_name.strip!
-        check_size = socket[0].gets
-        check_size.strip!
-        unless File.exist?(file_name)
-          file = File.open file_name, "wb"
-          while check_size.to_i > file.size
-            data = socket[0].gets
-            file.write data
+  p socket
+  begin
+    loop do
+      command = socket[0].gets
+      if command
+        case command.to_i 2
+        when 1
+          puts Time.now
+          socket[0].puts "Server time #{Time.now}"
+        when 2
+          line = socket[0].gets
+          puts line.strip!
+          socket[0].puts line.delete("ECHO ")
+        when 3
+          file_name = socket[0].gets
+          file_name.strip!
+          check_size = socket[0].gets
+          check_size.strip!
+          unless check_size.to_i == 0
+            file = File.open file_name, "wb"
+            while check_size.to_i > file.size
+              data = socket[0].gets
+              file.write data
+            end
+            file.close
           end
-          file.close
+        when 4
+          file_name = socket[0].gets
+          file_name.strip!
+          unless File.exist?(file_name)
+            puts "File don't exits!"
+            socket.puts 0
+          else
+            file = File.open file_name, "rb"
+            socket[0].puts file.size
+            data = file.read
+            socket[0].puts data
+            file.close
+          end
+        when 5
+          socket[0].close
+          break
+        when 6
+          socket[0].close
+          server.close
+          exit
         end
-      when 4
-        file_name = socket[0].gets
-        file_name.strip!
-        unless File.exist?(file_name)
-          puts "File don't exits!"
-          socket.puts 0
-        else
-          file = File.open file_name, "rb"
-          socket[0].puts file.size
-          data = file.read
-          socket[0].puts data
-          file.close
-        end
-      when 5
-        socket[0].close
-        break
-      when 6
-        socket[0].close
-        server.close
-        exit
       end
-  end
+    end
+  rescue => err
+    puts err
+    puts "Connection error! Reconnect? (y)"
+    ans = gets
+    unless ans.strip! == "y"
+      exit
+    else
+      redo
+      #socket[0].close
+      #server.listen(5)
+      #socket = server.accept
+      #p socket
+      #retry
+    end
   end
 end
