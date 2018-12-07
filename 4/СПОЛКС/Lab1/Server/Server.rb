00239e229287d060debe80c32d31441e228f81ea
SergeiKhaladtsou <SergeiKhaladtsou@gmail.com>
2018-12-08 01:30:51 +0300

    Correct upload data

--- 4/СПОЛКС/Lab1/Server/Server.rb
+++ 4/СПОЛКС/Lab1/Server/Server.rb
@@ -1,5 +1,6 @@
 require "socket"
 
+SIZE_PACKETH = 1024
 
 include Socket::Constants
 
@@ -22,67 +23,53 @@ loop do
   server.listen(5)
   socket = server.accept
   p socket
-  begin
-    loop do
-      command = socket[0].gets
-      if command
-        case command.to_i 2
-        when 1
-          puts Time.now
-          socket[0].puts "Server time #{Time.now}"
-        when 2
-          line = socket[0].gets
-          puts line.strip!
-          socket[0].puts line.delete("ECHO ")
-        when 3
-          file_name = socket[0].gets
-          file_name.strip!
-          check_size = socket[0].gets
-          check_size.strip!
-          unless check_size.to_i == 0
-            file = File.open file_name, "wb"
-            while check_size.to_i > file.size
-              data = socket[0].gets
-              file.write data
-            end
-            file.close
+  loop do
+    command = socket[0].gets
+    if command
+      case command.to_i 2
+      when 1
+        puts Time.now
+        socket[0].puts "Server time #{Time.now}"
+      when 2
+        line = socket[0].gets
+        puts line.strip!
+        socket[0].puts line.delete("ECHO ")
+      when 3
+        file_name = socket[0].gets
+        file_name.strip!
+        check_size = socket[0].gets
+        check_size.strip!
+        unless check_size.to_i == 0
+          file = File.open file_name, "wb"
+          quantity = socket[0].gets
+          last_packeth = check_size.to_i % SIZE_PACKETH
+          quantity.to_i.times do
+            file.write socket[0].read(SIZE_PACKETH)
           end
-        when 4
-          file_name = socket[0].gets
-          file_name.strip!
-          unless File.exist?(file_name)
-            puts "File don't exits!"
-            socket.puts 0
-          else
-            file = File.open file_name, "rb"
-            socket[0].puts file.size
-            data = file.read
-            socket[0].puts data
-            file.close
-          end
-        when 5
-          socket[0].close
-          break
-        when 6
-          socket[0].close
-          server.close
-          exit
+          file.write socket[0].read(last_packeth)
+          file.close
         end
+      when 4
+        file_name = socket[0].gets
+        file_name.strip!
+        unless File.exist?(file_name)
+          puts "File don't exits!"
+          socket.puts 0
+        else
+          file = File.open file_name, "rb"
+          socket[0].puts file.size
+          data = file.read
+          socket[0].puts data
+          file.close
+        end
+      when 5
+        socket[0].close
+        break
+      when 6
+        socket[0].close
+        server.close
+        exit
       end
-    end
-  rescue => err
-    puts err
-    puts "Connection error! Reconnect? (y)"
-    ans = gets
-    unless ans.strip! == "y"
-      exit
-    else
-      redo
-      #socket[0].close
-      #server.listen(5)
-      #socket = server.accept
-      #p socket
-      #retry
     end
   end
 end
