9bf479b6390ca45b7441a3730ff50734602c757e
SergeiKhaladtsou <SergeiKhaladtsou@gmail.com>
2018-12-17 19:16:26 +0300

    Correct resuming

--- 4/СПОЛКС/Lab1/Client/Client.rb
+++ 4/СПОЛКС/Lab1/Client/Client.rb
@@ -3,66 +3,6 @@ include Socket::Constants
 
 SIZE_PACKETH = 1024
 
-def resume_upload(file_name, packeth, client)
-  server_command = 7
-  unless File.exist?(file_name)
-    puts "File don't exist"
-  else
-    file = File.open file_name, "rb"
-    client.puts server_command
-    client.puts file_name
-    last_packeth = file.size % SIZE_PACKETH
-    client.puts last_packeth
-    quantity = file.size / SIZE_PACKETH
-    client.puts quantity
-    client.puts packeth
-    quantity.times do |pack|
-      data = file.read(SIZE_PACKETH)
-      next if pack < packeth
-      begin
-        client.write data
-      rescue
-        report = File.new "Error_report", "rb"
-        report.write 3
-        report.write file_name
-        report.write packeth
-        exit
-        #retry
-      end
-    end
-    client.write file.read(last_packeth)
-    file.close
-  end
-end
-
-def resume_download(file_name, packeth, client)
-  server_command = 8
-  client.puts server_command
-  client.puts file_name
-  client.puts packeth
-  file = File.open file_name, "ab"
-  last_packeth = file.size % SIZE_PACKETH
-  client.puts last_packeth
-  quantity = file.size / SIZE_PACKETH
-  client.puts quantity
-  quantity.times do |pack|
-    next if pack < packeth
-    begin
-      data = client.read(SIZE_PACKETH)
-    rescue
-      report = File.new "Error_report", "rb"
-      report.write 4
-      report.write file_name
-      report.write packeth
-      exit
-      #retry
-    end
-    file.write data
-  end
-  file.write client.read(last_packeth)
-  file.close
-end
-
 print "Input ip address server: "
 address = gets
 
@@ -76,32 +16,14 @@ client.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_KEEPINTVL, true)
 
 client.connect(sockaddr)
 loop do
-  unless File.exist?("Error_report")
-    puts "1. Time"
-    puts "2. Echo"
-    puts "3. Upload"
-    puts "4. Download"
-    puts "5. Disconnect"
-    puts "6. Close server and disconnect"
-    command = gets
-    command.strip!
-  else
-    puts "Resume ..."
-    report = File.open "Error_report", "rb"
-    command = report.readline
-    command.strip!
-    file_name = report.readline
-    file_name.strip!
-    packeth = report.readline
-    packeth.strip!
-    report.close
-    File.delete("Error_report")
-    if command.to_i == 3
-      resume_upload(file_name, packeth, client)
-    else
-      resume_download(file_name, packeth, client)
-    end
-  end
+  puts "1. Time"
+  puts "2. Echo"
+  puts "3. Upload"
+  puts "4. Download"
+  puts "5. Disconnect"
+  puts "6. Close server and disconnect"
+  command = gets
+  command.strip!
   client.puts command.to_i.to_s 2
   case command.to_i
   when 1
@@ -129,15 +51,7 @@ loop do
       start_time = Time.now
       quantity.times do |packeth|
         data = file.read(SIZE_PACKETH)
-        begin
-          client.write data
-        rescue
-          report = File.new "Error_report", "rb"
-          report.write 3
-          report.write file_name
-          report.write packeth
-          retry
-        end
+        client.write data
       end
       client.write file.read(last_packeth)
       file.close
@@ -181,5 +95,38 @@ loop do
   when 6
     client.close
     break
+  when 7
+    file_name = client.gets
+    last_packeth = client.gets
+    quantity = client.gets
+    file_name.strip!
+    last_packeth.strip!
+    quantity.strip!
+    packeth = client.gets
+    packeth.strip!
+    file = File.open file_name, "ab"
+    quantity.to_i.times do |pack|
+      next if pack < packeth.to_i
+      data = client.read(SIZE_PACKETH)
+      file.write data
+    end
+    file.write client.read(last_packeth.to_i)
+    file.close
+  when 8
+    file_name = client.gets
+    file_name.strip!
+    packeth = client.gets
+    packeth.strip!
+    last_packeth = client.gets
+    last_packeth.strip!
+    quantity = client.gets
+    quantity.strip!
+    file = File.open file_name, "rb"
+    quantity.to_i.times do |pack|
+      data = file.read(SIZE_PACKETH)
+      next if pack < packeth.to_i
+      client.puts data
+    end
+    client.puts file.read(last_packeth.to_i)
   end
 end
--- 4/СПОЛКС/Lab1/Server/Server.rb
+++ 4/СПОЛКС/Lab1/Server/Server.rb
@@ -4,6 +4,66 @@ SIZE_PACKETH = 1024
 
 include Socket::Constants
 
+def resume_upload(file_name, packeth, socket[0])
+  socket[0]_command = 7
+  unless File.exist?(file_name)
+    puts "File don't exist"
+  else
+    file = File.open file_name, "rb"
+    socket[0].puts server_command
+    socket[0].puts file_name
+    last_packeth = file.size % SIZE_PACKETH
+    socket[0].puts last_packeth
+    quantity = file.size / SIZE_PACKETH
+    socket[0].puts quantity
+    socket[0].puts packeth
+    quantity.times do |pack|
+      data = file.read(SIZE_PACKETH)
+      next if pack < packeth
+      begin
+        socket[0].write data
+      rescue
+        report = File.new "Error_report", "rb"
+        report.write 3
+        report.write file_name
+        report.write packeth
+        exit
+        #retry
+      end
+    end
+    socket[0].write file.read(last_packeth)
+    file.close
+  end
+end
+
+def resume_download(file_name, packeth, socket[0])
+  socket[0]_command = 8
+  socket[0].puts server_command
+  socket[0].puts file_name
+  socket[0].puts packeth
+  file = File.open file_name, "ab"
+  last_packeth = file.size % SIZE_PACKETH
+  socket[0].puts last_packeth
+  quantity = file.size / SIZE_PACKETH
+  socket[0].puts quantity
+  quantity.times do |pack|
+    next if pack < packeth
+    begin
+      data = socket[0].read(SIZE_PACKETH)
+    rescue
+      report = File.new "Error_report", "rb"
+      report.write 4
+      report.write file_name
+      report.write packeth
+      exit
+      #retry
+    end
+    file.write data
+  end
+  file.write socket[0].read(last_packeth)
+  file.close
+end
+
 print "Input your ip address: "
 address = gets
 
@@ -11,9 +71,6 @@ server = Socket.new(AF_INET, SOCK_STREAM, 0)
 server.bind(Addrinfo.tcp(address.strip!, "2000"))
 
 server.setsockopt(Socket::SOL_SOCKET, Socket::SO_KEEPALIVE, 30)
-server.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_KEEPCNT, true)
-server.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_KEEPIDLE, true)
-server.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_KEEPINTVL, true)
 
 p server.connect_address
 
@@ -22,7 +79,25 @@ loop do
   socket = server.accept
   p socket
   loop do
-    command = socket[0].gets
+    unless File.exist?("Error_report")
+      command = socket[0].gets
+    else
+      puts "Resume ..."
+      report = File.open "Error_report", "rb"
+      command = report.readline
+      command.strip!
+      file_name = report.readline
+      file_name.strip!
+      packeth = report.readline
+      packeth.strip!
+      report.close
+      File.delete("Error_report")
+      if command.to_i == 3
+        resume_upload(file_name, packeth, socket[0])
+      else
+        resume_download(file_name, packeth, socket[0])
+      end
+    end
     if command
       case command.to_i 2
       when 1
@@ -41,8 +116,17 @@ loop do
           file = File.open file_name, "wb"
           quantity = socket[0].gets
           last_packeth = check_size.to_i % SIZE_PACKETH
-          quantity.to_i.times do
-            file.write socket[0].read(SIZE_PACKETH)
+          quantity.to_i.times do |packeth|
+            data = socket[0].read(SIZE_PACKETH)
+            begin
+              file.write data
+            rescue
+              report = File.new "Error_report", "rb"
+              report.write 3
+              report.write file_name
+              report.write packeth
+              exit
+            end
           end
           file.write socket[0].read(last_packeth)
           file.close
@@ -71,39 +155,6 @@ loop do
         socket[0].close
         server.close
         exit
-      when 7
-        file_name = socket[0].gets
-        last_packeth = socket[0].gets
-        quantity = socket[0].gets
-        file_name.strip!
-        last_packeth.strip!
-        quantity.strip!
-        packeth = socket[0].gets
-        packeth.strip!
-        file = File.open file_name, "ab"
-        quantity.to_i.times do |pack|
-          next if pack < packeth.to_i
-          data = socket[0].read(SIZE_PACKETH)
-          file.write data
-        end
-        file.write socket[0].read(last_packeth.to_i)
-        file.close
-      when 8
-        file_name = socket[0].gets
-        file_name.strip!
-        packeth = socket[0].gets
-        packeth.strip!
-        last_packeth = socket[0].gets
-        last_packeth.strip!
-        quantity = socket[0].gets
-        quantity.strip!
-        file = File.open file_name, "rb"
-        quantity.to_i.times do |pack|
-          data = file.read(SIZE_PACKETH)
-          next if pack < packeth.to_i
-          socket[0].puts data
-        end
-        socket[0].puts file.read(last_packeth.to_i)
       end
     end
   end
